c0461109a638ec46df93286e552ee4e38d9f4634
AyobomiOmojola <ayomojola12@gmail.com>
2024-06-02 11:55:43 +0100

    testing

--- cypress/fixtures/Locators/successPage.js
+++ cypress/fixtures/Locators/successPage.js
@@ -1,5 +1,5 @@
 export const successPageLocators = {
     "success_header_text_selector":'[data-ui-id="page-title-wrapper"]',
     "success_order_number_container_selector":".columns .checkout-success > p > span",
-    "continue_shopping_btn_selector":".checkout-success .actionw"
+    "continue_shopping_btn_selector":".checkout-success .action"
 }
